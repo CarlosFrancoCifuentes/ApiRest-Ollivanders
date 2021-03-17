f8de2929e9a3cde32e7f5519570ee2cf683919b1
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-03-17 19:41:03 +0100

    build: clase bd

---
+++ repository/bd.py
@@ -0,0 +1,17 @@
+class BD():
+    inventario = [["+5 Dexterity Vest", 10, 20],
+                  ["Aged Brie", 2, 0],
+                  ["Elixir of the Mongoose", 5, 7],
+                  ["Sulfuras, Hand of Ragnaros", 0, 80],
+                  ["Sulfuras, Hand of Ragnaros", -1, 80],
+                  ["Backstage passes to a TAFKAL80ETC concert", 15, 20],
+                  ["Backstage passes to a TAFKAL80ETC concert", 10, 49],
+                  ["Backstage passes to a TAFKAL80ETC concert", 5, 49],
+                  # ["Conjured Mana Cake", 3, 6]
+                  ]
+
+    @staticmethod
+    def get_item(name):
+        items = BD.inventario
+        return (item for item in items if item[0] == name)
+        
